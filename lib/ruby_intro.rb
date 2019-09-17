500912e8692559f441a8b20ba57bb17231b3beb1
EC2 Default User <ec2-user@ip-172-31-28-7.us-east-2.compute.internal>
2019-09-17 16:47:24 +0000

    task 1 and 2 completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -15,6 +15,11 @@ def sum arr
 end
 
 def max_2_sum arr
+    if arr.size==0
+        return 0;
+    elsif arr.size==1
+        return arr[0];
+    end
     if arr[0] > arr[1]
         first = arr[0];
         sec = arr[1];
@@ -30,35 +35,57 @@ def max_2_sum arr
         elsif arr[n] > sec
             sec = arr[n]
         end
-        # print "first = ", first, " second = ", sec, "\n";
     end
     return first + sec
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? arr, target
+    if arr.size<=1
+        return false;
+    else
+        map = Hash.new;
+        for num in arr
+            if !map.has_key?(num)
+                map[target-num] = num
+            else return true;
+            end
+        end
+    end
+    return false;
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name;
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s.match(/^[^AEIOUaeiou\W]/);
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return  (s.size==0 || s.match(/[^01]/) || s[-1]=='1' || s[-2]=='1') ? false : true;
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    def initialize(isbn, price)
+        @_isbn = isbn
+        @_price = price.to_f
+        # assuming price is a number
+        if @_isbn=="" || @_price<=0
+            # exception
+        end
+        puts "A book (#{@_isbn}) of $#{@_price} is created!"
+    end
+    
+    def price_as_string
+        str = '%.2f' % @_price
+        return '$' + str
+    end
 end
 
-# arr = [4,2,7,9,8,12,10];
-# max_2_sum(arr);
-# print "sum = ",max_2_sum(arr), "\n";
+# b = BookInStock.new("123-88","22.774");
+# b.price_as_string
