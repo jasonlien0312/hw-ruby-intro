612b7e11a0d5f85c96bb536245d5f2ee0161693f
EC2 Default User <ec2-user@ip-172-31-28-7.us-east-2.compute.internal>
2019-09-17 03:53:20 +0000

    task one completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,11 +3,36 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    if arr.size==0 
+        return 0;
+    else 
+        sumV = 0;
+        for n in arr
+          sumV += n;
+        end
+    end
+    return sumV;
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    if arr[0] > arr[1]
+        first = arr[0];
+        sec = arr[1];
+    else
+        first = arr[1];
+        sec = arr[0];
+    end
+    
+    for n in 2..arr.size-1
+        if arr[n] > first
+            sec = first
+            first = arr[n]
+        elsif arr[n] > sec
+            sec = arr[n]
+        end
+        # print "first = ", first, " second = ", sec, "\n";
+    end
+    return first + sec
 end
 
 def sum_to_n? arr, n
@@ -33,3 +58,7 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+
+# arr = [4,2,7,9,8,12,10];
+# max_2_sum(arr);
+# print "sum = ",max_2_sum(arr), "\n";
